20b74bcb8ba48a8bfcfbcdcd136e32b72a94beb0
Paulina LH <65806675+paulinalh@users.noreply.github.com>
2021-09-15 10:28:26 -0500

    Cambio de velocidad proyectil y objetivos, juego infinito

--- juego_tiroParabolico.py
+++ juego_tiroParabolico.py
@@ -56,7 +56,7 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 3
 
     if inside(ball):
         speed.y -= 0.35
@@ -71,11 +71,11 @@ def move():
 
     draw()
 
-    for target in targets:
-        if not inside(target):
-            return
+    #for target in targets:
+        #if not inside(target):
+            #return
 
-    ontimer(move, 50)
+    ontimer(move, 20)
 
 
 setup(420, 420, 370, 0)
